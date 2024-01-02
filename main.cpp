c912d8d09182d157aab3a643d25b03bc99c483da
JuanMarceloT <jmarcelot2013@gmail.com>
2024-01-02 14:41:26 -0300

    Integrating file information with tokenization

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <string>
+
+struct FOODINFO {
+    char name[50];
+    int caloriesPer100g;
+};
 
 
 int main(){
@@ -14,6 +20,19 @@ int main(){
         return 1;
     }
 
+    while (fgets(line, sizeof(line), filePointer) != NULL) {
+        struct FOODINFO food;
+
+        char *token = strtok(line, ";");
+        if(token != NULL) {
+            strcpy(food.name, token);
+            token = strtok(NULL, ";"); 
+            if(token != NULL){
+               food.caloriesPer100g = std::stoi(token);
+            }
+        printf("Food: %s and calories: %d per 100 grams\n", food.name,food.caloriesPer100g);
+    }
+    }
     fclose(filePointer);
 
 
