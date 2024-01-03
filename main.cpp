3fd6249eb7e7b8ded7dd2e051e8d1158a9af9b01
JuanMarceloT <jmarcelot2013@gmail.com>
2024-01-03 15:51:48 -0300

    Receive command line arguments and generate output file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <string>
+#include <iostream>
+//avl e e abp
+
+
+
+
 
 struct FOODINFO {
     char name[50];
@@ -9,18 +15,17 @@ struct FOODINFO {
 };
 
 
-int main(){
-    FILE *filePointer;
+int main(int argc, char* argv[]){
+    FILE *SourceFIle;
     char line[100]; 
+    SourceFIle = fopen(argv[1], "r"); 
 
-    filePointer = fopen("1000Shuffled.csv", "r"); 
-
-    if (filePointer == NULL) {
+    if (SourceFIle == NULL) {
         printf("File opening failed.\n");
         return 1;
     }
 
-    while (fgets(line, sizeof(line), filePointer) != NULL) {
+    while (fgets(line, sizeof(line), SourceFIle) != NULL) {
         struct FOODINFO food;
 
         char *token = strtok(line, ";");
@@ -30,11 +35,23 @@ int main(){
             if(token != NULL){
                food.caloriesPer100g = std::stoi(token);
             }
-        printf("Food: %s and calories: %d per 100 grams\n", food.name,food.caloriesPer100g);
+        //printf("Food: %s and calories: %d per 100 grams\n", food.name,food.caloriesPer100g);
     }
     }
-    fclose(filePointer);
+    fclose(SourceFIle);
+    
 
 
+    FILE *OutputFile; 
+    OutputFile = fopen(argv[2], "w"); 
+
+    if (OutputFile == NULL) {
+        printf("Failed to create file.\n");
+        return 1;
+    }
+    fprintf(OutputFile,"%s" ,"test");
+
+
+    fclose(OutputFile);
     return 0;
 }
